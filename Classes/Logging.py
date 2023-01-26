79282a52e3f75b33d76d0a8520ef76c95bb5c72c
Job Thompson <pkwp38@gmail.com>
2023-01-25 21:20:21 -0500

    Updated logging file to use correct filepath
    Updated logging file to use correct filepath

--- Classes/Logging.py
+++ Classes/Logging.py
@@ -1,28 +1,37 @@
+from pprint import pprint
 import logging
 import datetime
 from Classes.Config import instanceConfig
+from ProgramState import programState
 
 # TODO: Re-Write to allow for multiple log types to be written to the same file and rewrite the log file creation.
 
-def write_to_log(info, level):
+def write_to_log(level, info):
     """Outputs info string to the log file that is set upon initial script execution."""
     logging.info(f'{str(datetime.datetime.now())} : {level} : {info}')  # Writes time and date as well as the info string to log file.
     return
-
 
 def create_logfile():
     """Creates a new Log file with a date Identifier. If there is an existing log file with that identifier,
     the function adds an iterative number to the end of the file name until it gets to a file name that doesn't' exist."""
     date = str(datetime.datetime.now().strftime("%m_%d_%Y")) # sets date to a variable
-    logfile = ('Log_' + date + '.log') # creates a new file name using date variable set above as the identifier
+    logfile = (f'Log_{date}.log') # creates a new file name using date variable set above as the identifier
     multiple = 1
     while True:  # compares the last log file to the created file name
         try:
-            open(instanceConfig.log_directory + logfile, "x") # creates a new file using the new log file name
+            open(f'{instanceConfig.log_directory}/{logfile}', "x") # creates a new file using the new log file name
             break
-        except Exception:  # if the file name is the same, it adds an iterating number to the end
-            logfile = ('Log_' + date + '_' + str(multiple) + '.log')
+        except Exception as e:  # if the file name is the same, it adds an iterating number to the end
+            pprint(e)
+            logfile = (f'Log_{date}_{str(multiple)}.log')
             multiple += 1  # iterates the number at the end of the file
-    logfile = (instanceConfig.log_directory  + logfile) # sets logfile to the full filepath.
+    logfile = (f'{instanceConfig.log_directory}/{logfile}') # sets logfile to the full filepath.
     logging.basicConfig(filename=logfile, level=logging.INFO) # sets the logging file as the newly created file.
     return logfile
+
+def handle_exit():
+    """This function handles the exit of the program. It writes a final log entry and closes the log file."""
+    write_to_log('INFO', f'Total Number of Attempts: {str(programState.RefreshAttempts)}')
+    write_to_log('INFO', 'Program Exited. Closing Log File.')
+    logging.shutdown()
+    exit(0)
