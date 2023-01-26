29e34e5196bac6d4bf49836308146c53c689ef3c
Job Thompson <pkwp38@gmail.com>
2023-01-25 20:50:17 -0500

    Created Custom URL Page
    Created Custom URL Page

---
+++ Classes/Pages/CustomURLPage.py
@@ -0,0 +1,30 @@
+import PySimpleGUI as sg
+from Classes.GUIButtons import gui_button, back_button, submit_button
+from Classes.ProductSelection import product_selection
+
+def handle_submit(submitted_url):
+    pass
+
+def custom_URL_page():
+    layout = [[sg.Text('Enter the URL for the product:')], [back_button(), submit_button()]]
+    window = sg.Window("URL Selection", layout, size=(400, 400), element_justification='c')
+
+    while True:
+        event, values = window.read()
+        
+        match event:
+            case sg.WIN_CLOSED:
+                exit(0)
+            
+            case 'Back':
+                break
+            
+            case 'Submit':
+                handle_submit(values[0])
+                break
+            
+            case _:
+                break
+            
+    window.close()
+    return
